4e2c83a4d16abdb3542123690da547012b7f4aa2
Zokhar <benderasd2001@mail.ru>
2025-02-27 19:00:14 +0900

    add DTO

---
+++ app/api/schemas/DaysOfWeek.py
@@ -0,0 +1,27 @@
+from itertools import count
+from dataclasses import dataclass, field
+
+
+from litestar.dto import DataclassDTO, DTOConfig
+
+
+@dataclass
+
+class DaysOfWeek:
+    id: int = field(init=False, default_factory=count().__next__)
+    day_name: str = field(default = None)
+
+
+
+
+
+class WriteDTO(DataclassDTO[Client]):
+    config = DTOConfig(exclude = {'id'})
+
+
+class ReadDTO(DataclassDTO[Client]):
+    config = DTOConfig(exclude = {''})
+
+
+class PatchDTO(DataclassDTO[Client]):
+    config = DTOConfig(exclude={'id'}, partial = True)
---
+++ app/api/schemas/RemainingSessions.py
@@ -0,0 +1,26 @@
+from itertools import count
+from dataclasses import dataclass, field
+
+
+from litestar.dto import DataclassDTO, DTOConfig
+
+
+@dataclass
+
+class RemainingSessions:
+    id: int = field(init=False, default_factory=count().__next__)
+    client_subsription_id: int = field(default = None)
+    sessions_remaining: int = field(default = None)
+    lust_update: str = field(default = None)
+
+
+class WriteDTO(DataclassDTO[Client]):
+    config = DTOConfig(exclude = {'id'})
+
+
+class ReadDTO(DataclassDTO[Client]):
+    config = DTOConfig(exclude = {''})
+
+
+class PatchDTO(DataclassDTO[Client]):
+    config = DTOConfig(exclude={'id'}, partial = True)
