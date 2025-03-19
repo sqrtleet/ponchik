5ccc0235293465d88b421d54d4402096684f3edc
sqrtleet <sqrtleet@gmail.com>
2025-03-20 02:01:27 +0900

    gg

--- app/api/schemas/scheduleDays.py
+++ app/api/schemas/scheduleDays.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass, field
 
 from litestar.dto import DataclassDTO, DTOConfig
 
-from app.api.enums.daysOfWeek import DaysOfWeek
+from app.api.enums.days_of_week import DaysOfWeek
 from app.api.schemas.schedule import Schedule
 
 
--- app/app.py
+++ app/app.py
@@ -3,8 +3,8 @@ from loguru import logger
 from litestar import Litestar, post, get, patch, put, delete
 from litestar.exceptions import HTTPException
 
-from core.db.db import plugin
+from .core.db.db import plugin
 
-from api.routes.client_controller import client_router
+from .api.routes.client_controller import client_router
 
 app = Litestar(route_handlers=[client_router], plugins=[plugin])
