851739b96ae0cefa54adc7ddef6762746025a879
sqrtleet <sqrtleet@gmail.com>
2025-03-19 18:49:39 +0900

    asd

--- app/app.py
+++ app/app.py
@@ -3,5 +3,8 @@ from loguru import logger
 from litestar import Litestar, post, get, patch, put, delete
 from litestar.exceptions import HTTPException
 
+from core.db.db import plugin
 
-app = Litestar(route_handlers=[])
+from api.routes.client_controller import client_router
+
+app = Litestar(route_handlers=[client_router], plugins=[plugin])
